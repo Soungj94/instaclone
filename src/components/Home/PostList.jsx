d22155252b2974d5068b9870deea92468898e7a6
정창원 <8500jcw@gmail.com>
2022-12-28 03:58:04 +0900

    :sparkles: feat 서버에서 데이터 가져와서 뿌려주는 포스트리스트 기능 완성

--- src/components/Home/PostList.jsx
+++ src/components/Home/PostList.jsx
@@ -1,17 +1,36 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
+import { __getPosts } from "../../redux/modules/mainSlice";
 import PostCard from "./PostCard";
 
 // 내가 등록한 글들을 나열해주는 컴포넌트
 const PostList = () => {
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   dispatch(__getPosts());
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(__getPosts());
+  }, [dispatch]);
 
-  return <PostCard />;
+  const { data } = useSelector((state) => state.mainSlice);
+  console.log(data?.posts);
+
+  return (
+    <StPostList>
+      {data?.posts.map((item) => {
+        return <PostCard key={item.postId} data={item} />;
+      })}
+    </StPostList>
+  );
 };
 
 export default PostList;
+
+const StPostList = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+`;
